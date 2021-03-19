f72dcaa10207adfc7c7856f7a6055fe1191e99d7
Andrii Shapoval <andrii.shapoval@smile-ukraine.com>
2021-03-19 14:08:25 +0200

    asdesed

--- client/src/pages/MoviesList.jsx
+++ client/src/pages/MoviesList.jsx
@@ -1,14 +1,55 @@
-import React, { Component } from 'react'
-import ReactTable from 'react-table'
-import api from '../api'
+import React, { Component } from 'react';
+import ReactTable from "react-table-6"; 
+import api from '../api';
 
 import styled from 'styled-components'
 
-import 'react-table/react-table.css'
+import "react-table-6/react-table.css";
 
 const Wrapper = styled.div`
     padding: 0 40px 40px 40px;
 `
+
+const Update = styled.div`
+    color: #ef9b0f;
+    cursor: pointer;
+`
+
+const Delete = styled.div`
+    color: #ff0000;
+    cursor: pointer;
+`
+
+class UpdateMovie extends Component {
+    updateUser = event => {
+        event.preventDefault()
+
+        window.location.href = `/movies/update/${this.props.id}`
+    }
+
+    render() {
+        return <Update onClick={this.updateUser}>Update</Update>
+    }
+}
+
+class DeleteMovie extends Component {
+    deleteUser = event => {
+        event.preventDefault()
+
+        if (
+            window.confirm(
+                `Do tou want to delete the movie ${this.props.id} permanently?`,
+            )
+        ) {
+            api.deleteMovieById(this.props.id)
+            window.location.reload()
+        }
+    }
+
+    render() {
+        return <Delete onClick={this.deleteUser}>Delete</Delete>
+    }
+}
 
 class MoviesList extends Component {
     constructor(props) {
@@ -24,6 +65,7 @@ class MoviesList extends Component {
         this.setState({ isLoading: true })
 
         await api.getAllMovies().then(movies => {
+            console.log(movies);
             this.setState({
                 movies: movies.data.data,
                 isLoading: false,
@@ -55,6 +97,28 @@ class MoviesList extends Component {
                 Header: 'Time',
                 accessor: 'time',
                 Cell: props => <span>{props.value.join(' / ')}</span>,
+            },
+            {
+                Header: '',
+                accessor: '',
+                Cell: function(props) {
+                    return (
+                        <span>
+                            <DeleteMovie id={props.original._id} />
+                        </span>
+                    )
+                },
+            },
+            {
+                Header: '',
+                accessor: '',
+                Cell: function(props) {
+                    return (
+                        <span>
+                            <UpdateMovie id={props.original._id} />
+                        </span>
+                    )
+                },
             },
         ]
 
